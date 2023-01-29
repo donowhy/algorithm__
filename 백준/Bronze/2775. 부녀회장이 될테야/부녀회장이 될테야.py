b82a6ee90b627c941a06637dc2a09c8783263945
donowhy <119389068+donowhy@users.noreply.github.com>
2023-01-29 21:55:41 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,10 +1,14 @@
 t = int(input())
 
-for _ in range(t):  
-    floor = int(input()) 
-    num = int(input())
-    f0 = [x for x in range(1, num+1)]
-    for k in range(floor): 
-        for i in range(1, num):  
-            f0[i] += f0[i-1]  
-    print(f0[-1])  
+dp = [[0] * 15 for _ in range(15)]
+dp[0] = [x for x in range(15)]
+
+for i in range(1,15):
+    for j in range(1, 15):
+        for k in range(1, j + 1):
+            dp[i][j] += dp[i-1][k]
+
+
+for _ in range(t):
+    k, n = int(input()), int(input())
+    print(dp[k][n])
