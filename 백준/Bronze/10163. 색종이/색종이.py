1b73274f50ccbcf45989a9c596c11d4d89a2493a
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-04 15:49:43 +0900

    [Bronze I] Title: 색종이, Time: 300 ms, Memory: 38436 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,24 @@
-import sys
-input = sys.stdin.readline
+from sys import stdin
 
-N = int(input())
-matrix = [[0]*1001 for _ in range(1001)]
-for k in range(1,N+1):
-    x,y,w,h = map(int, input().split())
-    for i in range(x,x+w):
-        for j in range(y,y+h):
-            matrix[i][j] = k
-cnt_color = [0] * (N+1)
-for i in range(1001):
-    for j in range(1001):
-        if matrix[i][j]:
-            cnt_color[matrix[i][j]] += 1
+n = int(stdin.readline())
+board = [[-1] * 1001 for _ in range(1001)]
+res = [0 for _ in range(n)]
+minx, miny = 1001, 1001
+maxx, maxy = 0, 0
 
-for i in range(1,N+1):
-    print(cnt_color[i])
+for k in range(n):
+    x, y, width, height = map(int, stdin.readline().split())
+    for i in range(x, x + width):
+        for j in range(y, y + height):
+            board[i][j] = k
+    minx, miny = min(x, minx), min(y, miny)
+    maxx, maxy = max(x + width, maxx), max(y + height, maxy)
+
+for k in range(n):
+    for i in range(minx, maxx):
+        for j in range(miny, maxy):
+            if board[i][j] == k:
+                res[k] += 1
+
+for i in res:
+    print(i)
