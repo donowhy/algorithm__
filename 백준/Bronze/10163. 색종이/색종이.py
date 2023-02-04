82a13792940f9b66163cc1564a4e0535ec7ac616
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-04 16:06:38 +0900

    [Bronze I] Title: 색종이, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,24 +1,14 @@
-from sys import stdin
-
-n = int(stdin.readline())
-board = [[-1] * 1001 for _ in range(1001)]
-res = [0 for _ in range(n)]
-minx, miny = 1001, 1001
-maxx, maxy = 0, 0
-
-for k in range(n):
-    x, y, width, height = map(int, stdin.readline().split())
-    for i in range(x, x + width):
-        for j in range(y, y + height):
-            board[i][j] = k
-    minx, miny = min(x, minx), min(y, miny)
-    maxx, maxy = max(x + width, maxx), max(y + height, maxy)
-
-for k in range(n):
-    for i in range(minx, maxx):
-        for j in range(miny, maxy):
-            if board[i][j] == k:
-                res[k] += 1
-
-for i in res:
-    print(i)
+s = [[0]*101 for i in range(101)] # 빈 격자판을 만든다.
+n = int(input())
+# 테스트 케이스 수만큼 색종이별로 테스트 케이스에 해당하는 숫자(i+1)로 채운다.
+for i in range(n):
+    x, y, w, h = map(int, input().split())
+    for j in range(x, x+w):
+        for k in range(y, y+h):
+            s[j][k] = i+1
+for i in range(n): # 테스트 케이스 수만큼 돌며 각 색종이 면적 출력.
+    cnt = 0
+    for m in s:
+        cnt += m.count(i+1)
+        # print(cnt)
+    print(cnt)
