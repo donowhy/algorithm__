7fdc2a113687ac70101da0d7bfed5896237e6e88
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-06 22:27:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 704 ms, Memory: 38416 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+lst = [[0] * N for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(lambda x: x - 1, map(int,input().split()))
+    lst[a][b] = lst[b][a] = 1
+
+ans = 0
+chk = [False] * N
+
+def dfs(now):
+    for nxt in range(N):
+        if lst[now][nxt] and not chk[nxt]:
+            chk[nxt] = True
+            dfs(nxt)
+
+for i in range(N):
+    if not chk[i]:
+        ans += 1
+        chk[i] = True
+        dfs(i)
+
+print(ans)
