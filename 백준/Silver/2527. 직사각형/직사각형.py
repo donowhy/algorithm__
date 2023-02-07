ca4e83c08d9d8b85f3ca2f839ca9e1e268c18665
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-07 17:32:22 +0900

    [Silver I] Title: 직사각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(4):
+    x1,y1,p1,q1,x2,y2,p2,q2 = map(int, input().split())
+    if p1 < x2 or p2 < x1 or y1 > q2 or q1 < y2:
+        print('d')
+        continue
+    elif x1==p2 or x2==p1:
+        if q1==y2 or q2==y1:
+            print('c')
+            continue
+        else:
+            print('b')
+            continue
+    elif q1==y2 or q2==y1:
+            print('b')
+            continue
+    else:
+        print('a')
+        continue
