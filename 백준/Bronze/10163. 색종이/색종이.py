8d8ee1ba4873280b6d01e86e5c951271c6d2b376
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-09 15:27:38 +0900

    [Bronze I] Title: 색종이, Time: 4808 ms, Memory: 122972 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,12 +1,15 @@
-s = [[0]*101 for i in range(101)] # 빈 격자판을 만든다.
+import sys
+input = sys.stdin.readline
+
+s = [[0]*1001 for i in range(1001)]
 n = int(input())
-# 테스트 케이스 수만큼 색종이별로 테스트 케이스에 해당하는 숫자(i+1)로 채운다.
+
 for i in range(n):
     x, y, w, h = map(int, input().split())
     for j in range(x, x+w):
         for k in range(y, y+h):
             s[j][k] = i+1
-for i in range(n): # 테스트 케이스 수만큼 돌며 각 색종이 면적 출력.
+for i in range(n): 
     cnt = 0
     for m in s:
         cnt += m.count(i+1)
