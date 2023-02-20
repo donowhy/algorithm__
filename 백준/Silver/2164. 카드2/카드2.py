553f5a176b4071f5b192fd9725a766f5e79e37c6
donowhy <119389068+donowhy@users.noreply.github.com>
2023-02-20 13:47:00 +0900

    [Silver IV] Title: 카드2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,11 @@
-from collections import deque
+input = int(input())
+square = 2
 
-N = int(input())
-deque = deque([i for i in range(1, N+1)])
-
-while(len(deque) >1):
-    deque.popleft()
-    move_num = deque.popleft()
-    deque.append(move_num)
-    
-print(deque[0])
+while True:
+    if (input == 1 or input == 2):
+        print(input)
+        break
+    square *= 2
+    if (square >= input):
+        print((input - (square // 2)) * 2)
+        break
