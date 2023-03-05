1e36d0ac1bdd3819225a2b49043a9b0f9ed45506
donowhy <119389068+donowhy@users.noreply.github.com>
2023-03-05 18:05:36 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,19 +1,12 @@
-n = int(input())
+N = int(input())
 
-if n % 5 == 0: 
-    print(n // 5)
-else:
-    p = 0
-    while n > 0:
-        n -= 3
-        p += 1
-        if n % 5 == 0: 
-            p += n // 5
-            print(p)
-            break
-        elif n == 1 or n == 2:
-            print(-1)
-            break
-        elif n == 0:
-            print(p)
-            break
+cnt = 0
+while N >= 0 :
+    if N % 5 == 0 : 
+        cnt += (N // 5) 
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1  
+else :
+    print(-1)
