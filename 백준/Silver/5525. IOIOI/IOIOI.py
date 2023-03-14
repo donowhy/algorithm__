0da1c964a14d206b0e0b786820f7bba20f4393c5
donowhy <119389068+donowhy@users.noreply.github.com>
2023-03-14 11:32:01 +0900

    [Silver I] Title: IOIOI, Time: 136 ms, Memory: 114988 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,7 +4,8 @@ M = int(input())
 arr = list(input())
 stk = []
 cnt = 0
+a = 'I' + 'OI' * N
 for i in range(len(arr) - (N*2)):
-    if ''.join(arr[i:i+(N*2+1)]) == 'I' + 'OI' * N:
+    if ''.join(arr[i:i+(N*2+1)]) == a:
         cnt += 1
 print(cnt)
