93987357a50d03fc3c367388a046ea7fd86da6d9
donowhy <119389068+donowhy@users.noreply.github.com>
2023-04-07 16:38:21 +0900

    [Gold V] Title: 치킨 배달, Time: 364 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,28 +1,49 @@
-from itertools import combinations
-N, M = map(int,input().split())
-
-arr = []
-chik = []
-
-for i in range(N):
-    for j, v in enumerate(map(int,input().split())):
-        if v == 1:
-            arr.append((i, j))
-        elif v == 2:
-            chik.append((i, j))
-
-def get_dist(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+from collections import deque
+N, M = map(int, input().split())                                # 도시 크기, 남은 치킨집 수
+arr = [list(map(int, input().split())) for _ in range(N)]       # 도시
 
 
-ans = 2 * N * len(arr)
-for combi in combinations(chik, M):
-    tot = 0 
-    for arrs in arr:
-        tot += min(get_dist(arrs, chic) for chic in combi)
+def dfs(n, i):      # n: 고른 치킨집 수 i: 고른 치킨집 번호
+    global result
+    val = 0
+    # 모든 치킨집을 골랐다면 치킨거리 계산
+    if n == M:
+        for h in house:
+            hr, hc = h[0], h[1]
+            dist = 2*N
 
+            for c in select:
+                cr, cc = c[0], c[1]
+                tmp = abs(hr-cr) + abs(hc-cc)
+                if tmp < dist:
+                    dist = tmp
+            val += dist
 
-    ans = min(ans, tot)
+        if val < result:
+            result = min(val, result)
+            return
+	# 고른 치킨 집을 제외하고 dfs
+    for idx in range(i, K):
+        select.append(chicken[idx])
+        dfs(n+1, idx+1)
+        select.pop()
 
-print(ans)
+chicken = deque([])
+house = deque([])
 
+select = deque([])
+for a in range(N):
+    for b in range(N):
+        if arr[a][b] == 1:        # 집 위치 추가
+            house.append((a, b))
+        elif arr[a][b] == 2:      # 치킨집 위치 추가
+            chicken.append((a, b))
+
+K = len(chicken)             # 총 치킨집 수
+result = N*2*len(house)      # 총 치킨거리 임의의 큰 값
+
+# 조합 시작
+for t in range(K):
+    dfs(0, t)
+
+print(result)
