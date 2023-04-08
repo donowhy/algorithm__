e29e6a5d11aea514735b03d830572e5b62c6e672
donowhy <119389068+donowhy@users.noreply.github.com>
2023-04-08 12:36:13 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 464 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,54 +1,69 @@
-# 입력
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-moves = [tuple(map(int, input().split())) for _ in range(M)]
+# 삼성 오후 1번 문제
+# 구현
 
-# 8방향
-dy8 = ("empty", 0, -1, -1, -1, 0, 1, 1, 1)
-dx8 = ("empty", -1, -1, 0, 1, 1, 1, 0, -1)
+n, m = map(int, input().split())
 
-# 대각 4방향
-dy4 = (-1, -1, 1, 1)
-dx4 = (-1, 1, -1, 1)
+arr = [list(map(int, input().split())) for _ in range(n)]
+moves = []
+for i in range(m):
+    tmp = list(map(int, input().split()))
+    moves.append([tmp[0] - 1, tmp[1]])
 
-clouds = [(N - 1, 0), (N - 1, 1), (N - 2, 0), (N - 2, 1)]  # 구름 좌표
-for d, s in moves:
-    # 모든 구름 이동
-    moved_clouds = []
-    for y, x in clouds:
-        # 구름들을 d 방향으로 s만큼 이동(구름의 좌표는 연결되어있으므로 %N)
-        ny = (y + dy8[d] * s) % N
-        nx = (x + dx8[d] * s) % N
-        board[ny][nx] += 1  # 물의 양 추가
-        moved_clouds.append((ny, nx))  # 이동한 구름 좌표에 추가
+clouds = [[n - 2, 0], [n - 2, 1], [n - 1, 0], [n - 1, 1]]
 
-    for y, x in moved_clouds:
-        # 이동한 구름들의 대각 4방향을 조사하여 count만큼 물의 양 추가    
-        cnt = 0
-        for d in range(4):
-            ny = y + dy4[d]
-            nx = x + dx4[d]
-            # 이 때는 구름의 좌표가 연결되어 있지 않으므로 예외처리)
-            if ny < 0 or nx < 0 or ny >= N or nx >= N:
-                continue
-            elif board[ny][nx] > 0:
-                cnt += 1
-        board[y][x] += cnt
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+for i in range(m):
+    # step 1.
+    # 이동
+    move = moves[i]
+    next_clouds = []
+    for cloud in clouds:
+        x = cloud[0]
+        y = cloud[1]
+        d = move[0]
+        s = move[1]
+        nx = (n + x + dx[d] * s) % n
+        ny = (n + y + dy[d] * s) % n
+        next_clouds.append([nx, ny])
 
-    new_clouds = []
-    for y in range(N):
-        for x in range(N):
-            # 이동한 구름의 좌표와 동일하지 않고, 물의 양이 2 이상인 경우
-            if (y, x) in moved_clouds or board[y][x] < 2:
-                continue
-            # 물을 2만큼 소비하고 새로운 구름 배열(new_clouds)에 추가
-            board[y][x] -= 2
-            new_clouds.append((y, x))
-    clouds = new_clouds  # 다음 loop에서 사용할 clouds 배열을 new_clouds로 교체
+    # step 2.
+    visited = [[False] * n for _ in range(n)]
+    for cloud in next_clouds:
+        x = cloud[0]
+        y = cloud[1]
+        arr[x][y] += 1
+        visited[x][y] = True
 
-# 물의 양 합 계산 후 출력
-result = 0
-for y in range(N):
-    for x in range(N):
-        result += board[y][x]
-print(result)
+    # step 3
+    clouds = []
+
+    # step 4
+    cx = [-1, -1, 1, 1]
+    cy = [-1, 1, -1, 1]
+    for cloud in next_clouds:
+        x = cloud[0]
+        y = cloud[1]
+        count = 0
+        for i in range(4):
+            nx = x + cx[i]
+            ny = y + cy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] >= 1:
+                count += 1
+
+        arr[x][y] += count
+
+    # step 5
+
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] >= 2 and visited[i][j] == False:
+                arr[i][j] -= 2
+                clouds.append([i, j])
+
+ans = 0
+for i in range(n):
+    ans += sum(arr[i])
+
+print(ans)
