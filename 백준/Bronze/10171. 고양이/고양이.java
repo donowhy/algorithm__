534eb13fe40386412975f182465e5f1c072f920c
donowhy <119389068+donowhy@users.noreply.github.com>
2023-04-16 14:53:46 +0900

    [Bronze V] Title: 고양이, Time: 76 ms, Memory: 11240 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\"); // 역슬래시 출력을 위해 \를 두 번 입력해야 함
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
