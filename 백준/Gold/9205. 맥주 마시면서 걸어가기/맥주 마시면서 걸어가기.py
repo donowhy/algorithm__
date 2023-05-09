dfdbba2e1c74a28ab05c16ca48cd1151f4b2be78
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-09 19:11:58 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 196 ms, Memory: 116604 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,21 +1,26 @@
+from collections import deque
 import sys
+
+def bfs(start, end):
+    queue = deque()
+    queue.append(start)
+    visited = set()
+    visited.add(start)
+
+    while queue:
+        x, y = queue.popleft()
+        if (x, y) == end:
+            return "happy"
+
+        for nx, ny in coord:
+            if (nx, ny) not in visited and abs(nx - x) + abs(ny - y) <= 1000:
+                visited.add((nx, ny))
+                queue.append((nx, ny))
+
+    return "sad"
 
 T = int(sys.stdin.readline())
 for _ in range(T):
     n = int(sys.stdin.readline())
     coord = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n+2)]
-    dist = [[0 if i==j else float('inf') for j in range(n+2)] for i in range(n+2)]
-
-    for i in range(n+2):
-        for j in range(i+1, n+2):
-            if abs(coord[i][0]-coord[j][0]) + abs(coord[i][1]-coord[j][1]) <= 1000:
-                dist[i][j] = 1
-                dist[j][i] = 1
-
-    for k in range(n+2):
-        for i in range(n+2):
-            for j in range(n+2):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-
-    print("happy" if dist[0][-1] != float('inf') else "sad")
+    print(bfs(coord[0], coord[-1]))
