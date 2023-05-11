1ae831a4065bef1df0a7a8473444511ef275ccc0
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-11 18:42:31 +0900

    [Gold IV] Title: 알고스팟, Time: 144 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,38 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int M, N;
-    static int[][] map, dist;
-    static boolean[][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
+    static int N, M;
+    static int[][] map;
+    static int[][] dist;
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+    
     static class Node implements Comparable<Node> {
         int x, y, dist;
-
+        
         public Node(int x, int y, int dist) {
             this.x = x;
             this.y = y;
             this.dist = dist;
         }
-
+        
         @Override
-        public int compareTo(Node o) {
-            return this.dist - o.dist;
+        public int compareTo(Node other) {
+            return this.dist - other.dist;
         }
     }
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
         dist = new int[N][M];
-        visited = new boolean[N][M];
-
+        
         for(int i=0; i<N; i++) {
             String line = br.readLine();
             for(int j=0; j<M; j++) {
@@ -41,32 +39,29 @@ public class Main {
                 dist[i][j] = Integer.MAX_VALUE;
             }
         }
-
-        dijkstra();
+        
+        bfs();
         System.out.println(dist[N-1][M-1]);
     }
-
-    static void dijkstra() {
+    
+    static void bfs() {
         PriorityQueue<Node> queue = new PriorityQueue<>();
         queue.offer(new Node(0, 0, 0));
         dist[0][0] = 0;
-
+        
         while(!queue.isEmpty()) {
             Node node = queue.poll();
             int x = node.x;
             int y = node.y;
             int current_dist = node.dist;
-
-            if(visited[x][y]) continue;
-            visited[x][y] = true;
-
+            
             for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
+                
                 if(nx>=0 && ny>=0 && nx<N && ny<M) {
                     int next_dist = current_dist + map[nx][ny];
-
+                    
                     if(next_dist < dist[nx][ny]) {
                         dist[nx][ny] = next_dist;
                         queue.offer(new Node(nx, ny, next_dist));
