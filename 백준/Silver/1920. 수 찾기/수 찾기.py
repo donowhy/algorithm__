3fa1cb31796bba92b9a68154fe22a43e443f0b6f
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-14 05:27:23 +0900

    [Silver IV] Title: 수 찾기, Time: 260 ms, Memory: 141236 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,15 +1,33 @@
 import sys
 
-input = sys.stdin.readline
+readline = sys.stdin.readline
 
 N = int(input())
-lst = set(map(int, input().split()))
+la = sorted(list(map(int,input().rstrip().split())))
 
 M = int(input())
-arr = list(map(int, input().split()))
+lb = list(map(int,input().rstrip().split()))
 
-for i in arr:
-    if i in lst:
-        print(1)
-    else:
+
+for i in lb:
+    start = 0
+    end = N - 1
+    flag = 0
+    while start < end:
+        mid = (start + end) // 2
+
+        if i < la[start] or i > la[end]:
+            break
+
+        if la[mid] == i or la[start] == i or la[end] == i:
+            flag = 1
+            print(1)
+            break
+        elif la[mid] > i:
+            end = mid
+        else:
+            start = mid + 1
+
+
+    if flag == 0:
         print(0)
