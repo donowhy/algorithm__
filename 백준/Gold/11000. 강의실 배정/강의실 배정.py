ff066490a5520593734b706e648db1df9b52f489
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-14 14:22:44 +0900

    [Gold V] Title: 강의실 배정, Time: 428 ms, Memory: 86652 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,20 +1,17 @@
 import sys
-import heapq
+import heapq as hq
 
+n = int(sys.stdin.readline())
+meeting = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-heap = []
-input = sys.stdin.readline
-n = int(input())
-arr = []
-for i in range(n):
-    a, b = map(int, input().split())
-    arr.append([a, b])
-arr.sort(key=lambda x: x[0])
-heapq.heappush(heap, arr[0][1])
+meeting.sort(key=lambda x: (x[0], x[1]))
+
+rooms = []
+hq.heappush(rooms, meeting[0][1])
+
 for i in range(1, n):
-    if heap[0] > arr[i][0]:
-        heapq.heappush(heap, arr[i][1])
-    else:
-        heapq.heappop(heap)
-        heapq.heappush(heap, arr[i][1])
-print(len(heap))
+    if meeting[i][0] >= rooms[0]:
+        hq.heappop(rooms)
+    hq.heappush(rooms, meeting[i][1])
+
+print(len(rooms))
