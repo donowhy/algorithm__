08c92bae0db2f9886c0b13035f7d3c999cfc6d0c
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-17 20:13:30 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,25 @@
 from collections import deque
 
-dy = (0, 1, 0, -1)
-dx = (1, 0, -1, 0)
+def bfs(start, end):
+    dx = (1, 0, -1, 0)
+    dy = (0, 1, 0, -1)
 
-N, M = map(int, input().split())
-board = [input() for _ in range(N)]
+    q = deque()
+    q.append((start, end))
 
+    while q:
+        x, y = q.popleft()
 
-def is_valid_coord(y, x):
-    return 0 <= y < N and 0 <= x < M
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            if 0 <= nx < n and 0 <= ny < m and mapp[nx][ny] == 1:
+                q.append((nx,ny))
+                mapp[nx][ny] = mapp[x][y] + 1
 
-def bfs():
-    pass
-    chk = [[False] * M for _ in range(N)]
-    chk[0][0] = True
-    dq = deque()
-    dq.append((0, 0, 1))  # 마지막 1은 몇 칸을 지나왔는지, 시작점도 포함하니까 1로 시작
-    while dq:
-        y, x, d = dq.popleft()
-        if y == N - 1 and x == M - 1:
-            return d
+n, m = map(int, input().split())
+mapp = [list(map(int, input())) for _ in range(n)]
 
-        nd = d + 1
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:  # 문자열로 저장했기 때문에 문자열 1
-                chk[ny][nx] = True
-                dq.append((ny, nx, nd))
-
-
-print(bfs())
+bfs(0,0)
+print(mapp[n-1][m-1])
