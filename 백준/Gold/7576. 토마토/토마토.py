7a1707bbb26fe4423003d663b469ee15daa4e131
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-22 20:58:21 +0900

    [Gold V] Title: 토마토, Time: 1340 ms, Memory: 106900 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,55 +1,39 @@
-# bfs 특 queue 사용하기
-# deque 모듈 안쓰면 시간복잡도 박살남(pop(0)이 시간복잡도가 O(n)이고 popleft()가 O(1)이라고 함)
 from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs():
+    while queue:
+        a, b = queue.popleft()
+        for i in range(4):
+            x = a + dx[i]
+            y = b + dy[i]
+            if 0 <= x < n and 0 <= y < m and tomato[x][y] == 0:
+                tomato[x][y] = tomato[a][b] + 1
+                queue.append([x, y])
 
 m, n = map(int, input().split())
-# 토마토 받아서 넣기. 이차원 리스트로 만들어질거.
-matrix = [list(map(int, input().split())) for _ in range(n)]
-# 좌표를 넣을거니까 []를 넣자.
-dq= deque([])
-# 방향 리스트. [dx[0], dy[0]]은 곧 [-1, 0]이고 이는 왼쪽으로 이동하는 위치이다.
-# 그려보면 이해하기 편함
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-# 정답이 담길 변수
-result = 0
+tomato = []
+queue = deque()
 
-# dq에 처음에 받은 토마토의 위치 좌표를 append 시킨다
-# n과 m을 사용하는걸 헷갈리지 말아야 함!
 for i in range(n):
+    row = list(map(int, input().split()))
     for j in range(m):
-        if matrix[i][j] == 1:
-            # print(i, j)
-            dq.append([i, j])
-
-# bfs 함수. 한번 들어가면 다 돌고 나오니까 재귀 할 필요 없음
-def bfs():
-    while dq:
-        # 처음 토마토 좌표 x, y에 꺼내고
-        x, y = dq.popleft()
-        # print(x, y)
-        # 처음 토마토 사분면의 익힐 토마토들을 찾아본다.
-        for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            # 해당 좌표가 좌표 크기를 넘어가면 안되고, 그 좌표에 토마토가 익지 않은채로 있어야 함(0).
-            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
-                # 익히고 1을 더해주면서 횟수를 세어주기
-                # 여기서 나온 제일 큰 값이 정답이 될 것임
-                matrix[nx][ny] = matrix[x][y] + 1
-                dq.append([nx, ny])
+        if row[j] == 1:
+            queue.append([i, j])
+    tomato.append(row)
 
 bfs()
 
-for i in matrix:
-    # print(i,'i')
+max_day = 0
+for i in tomato:
     for j in i:
-        # print(j, 'j')
-        # 다 찾아봤는데 토마토를 익히지 못했다면 -1 출력
         if j == 0:
             print(-1)
             exit(0)
-    # 다 익혔다면 최댓값이 정답
-    result = max(result, max(i))
-# 처음 시작을 1로 표현했으니 1을 빼준다.
-print(result - 1)
+    max_day = max(max_day, max(i))
 
+print(max_day - 1)
