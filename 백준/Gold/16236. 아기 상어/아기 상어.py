84f2a6e905645dad2115fc789daf9c8d1af03c52
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-30 01:50:09 +0900

    [Gold III] Title: 아기 상어, Time: 164 ms, Memory: 34804 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,68 +1,56 @@
-import copy
- 
-def findTopLeft(queue, n):
-  tmp = []
-  for x, y in queue:
-    tmp.append(x*n + y)
-  tmp.sort()
-  X = tmp[0] // n
-  Y = tmp[0] % n
-  return X, Y
- 
-def isAvailable(x, y, arr, size, n, history):
-  if 0 <= x < n and 0 <= y < n:
-    if history[x][y] != 1:
-      if arr[x][y] <= size:
-        return True
-  return False
- 
-def calc(n, start, arr, size):
-  queue = [start]
-  a, b = start
-  history = [[0] * n for _ in range(n)]
-  history[a][b] = 1
-  for length in range(1, n*n):
-    nextQueue = []
-    eatableQueue = []
-    for x, y in queue:
-      for p, q in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        if isAvailable(x+p, y+q, arr, size, n, history):
-          nextQueue.append((x+p, y+q))
-          history[x+p][y+q] = 1
-          if 0 < arr[x+p][y+q] < size:
-            eatableQueue.append((x+p, y+q))
-    if len(eatableQueue) != 0:
-      x, y = findTopLeft(eatableQueue, n)
-      arr[a][b] = 0
-      arr[x][y] = 9
-      return length, x, y
-    if len(nextQueue) == 0:
-      return 0, 0, 0
-    queue = copy.deepcopy(nextQueue)
- 
-def solve():
-  n = int(input())
-  arr = []
-  for i in range(n):
-    line = [int(x) for x in input().split()]
+from collections import deque
+import heapq
+
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+
+def bfs(x, y, size):
+    visited = [[False]*n for _ in range(n)]
+    queue = deque([(0, x, y)])
+    heap = []
+    visited[x][y] = True
+
+    while queue:
+        dist, x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                # 아기 상어가 지나갈 수 있는 경우
+                if mapp[nx][ny] <= size:
+                    visited[nx][ny] = True
+                    queue.append((dist+1, nx, ny))
+                    # 물고기를 먹을 수 있는 경우
+                    if 0 < mapp[nx][ny] < size:
+                        heapq.heappush(heap, (dist+1, nx, ny))
+    if heap:
+        return heapq.heappop(heap)
+    else:
+        return None
+
+n = int(input())
+mapp = [list(map(int, input().split())) for _ in range(n)]
+
+x, y = 0, 0
+for i in range(n):
     for j in range(n):
-      if line[j] == 9:
-        start = (i, j)
+        if mapp[i][j] == 9:
+            x, y = i, j
+            mapp[i][j] = 0
+
+time, size, feed = 0, 2, 0
+while True:
+    result = bfs(x, y, size)
+    if result is None:
+        print(time)
         break
-    arr.append(line)
-  rst = 0
-  size = 2
-  cnt = 0
-  while True:
-    ret, a, b = calc(n, start, arr, size)
-    if ret == 0:
-      print(rst)
-      return
-    cnt += 1
-    if cnt == size:
-      size += 1
-      cnt = 0
-    rst += ret
-    start = (a, b)
- 
-solve()
+    else:
+        dist, x, y = result
+        time += dist
+        feed += 1
+        mapp[x][y] = 0
+        if feed == size:
+            size += 1
+            feed = 0
