501aa3f10e9ba3a5f8afbaf0a09392bacef11723
donowhy <119389068+donowhy@users.noreply.github.com>
2023-05-31 01:23:09 +0900

    [Silver II] Title: 스택 수열, Time: 252 ms, Memory: 133596 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,20 +1,27 @@
 n = int(input())
-stack = []
-answer = []
-cur = 1
-for i in range(n):
-    num = int(input())
-    while cur <= num:      
-        stack.append(cur)
-        answer.append("+")
-        cur += 1
-  
-    if stack[-1] == num:  
-        stack.pop()       
-        answer.append("-")
-    else:                   
-        print("NO")   
-        exit()
 
-for i in answer:
-    print(i)
+lst = []
+res = []
+
+for i in range(n):
+    tmp = int(input())
+    res.append(tmp)
+
+idx = 0
+
+answer = []
+
+for i in range(1, n+1):
+    lst.append(i)
+    answer.append('+')
+
+    while lst and lst[-1] == res[idx]:
+        idx += 1
+        lst.pop()
+        answer.append('-')
+
+if idx == n:
+    for i in range(len(answer)):
+        print(answer[i])
+else:
+    print('NO')
