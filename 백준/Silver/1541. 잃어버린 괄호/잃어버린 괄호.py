07f49ae2d0a0c4fec0562217886e291b3fbac299
donowhy <119389068+donowhy@users.noreply.github.com>
2023-06-08 20:08:53 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,12 +1,15 @@
-a = input().split('-')
-num = []
-for i in a:
+s = input().split('-')
+num_list = []
+
+for i in s:
     cnt = 0
-    s = i.split('+')
-    for j in s:
+    s2 = i.split('+')
+    for j in s2:
         cnt += int(j)
-    num.append(cnt)
-n = num[0]
-for i in range(1, len(num)):
-    n -= num[i]
+    num_list.append(cnt)
+
+n = num_list[0]
+for i in range(1, len(num_list)):
+    n -= num_list[i]
+
 print(n)
