4894502a724dc68531df29c6644ae309a91fbd91
donowhy <119389068+donowhy@users.noreply.github.com>
2023-06-12 02:18:46 +0900

    [Gold III] Title: 줄 세우기, Time: 1068 ms, Memory: 55940 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,52 +4,54 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[] edgeCount;
+    static ArrayList<Integer> res = new ArrayList<>();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N, M;
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[] edgeCount = new int[N+1];
+        edgeCount = new int[N+1];
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-        for(int i = 0; i < N+1; i++){
-            graph.add(new ArrayList<Integer>());
+        for(int i=0; i<N+1; i++){
+            graph.add(new ArrayList<>());
         }
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph.get(a).add(b);
-            edgeCount[b]++;
 
+            graph.get(a).add(b);
+            edgeCount[b] += 1;
         }
 
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i=1; i<edgeCount.length; i++){
-            if(edgeCount[i] == 0){
-                q.offer(i);
+        for(int i=0; i < edgeCount.length; i++){
+            if(edgeCount[i] ==0){
+                pq.add(i);
             }
         }
-        ArrayList<Integer> array = new ArrayList<>();
-        while(!q.isEmpty()){
-            int nodeNo = q.poll();
-            array.add(nodeNo);
 
-            List<Integer> list = graph.get(nodeNo);
+        while(!pq.isEmpty()){
+            int Node = pq.poll();
+            res.add(Node);
+            List<Integer> list = graph.get(Node);
 
-            for(int i=0; i < list.size(); i++){
-                edgeCount[list.get(i)] -- ;
-
+            for(int i=0; i<graph.get(Node).size(); i++){
+                edgeCount[list.get(i)]--;
                 if(edgeCount[list.get(i)] == 0){
-                    q.offer(list.get(i));
+                    pq.offer(list.get(i));
                 }
             }
         }
-        for(int i=0; i< array.size();i++){
-            System.out.print(array.get(i) + " ");
+        for(int i=1; i<res.size(); i++){
+            System.out.print(res.get(i) + " ");
         }
+
     }
 }
