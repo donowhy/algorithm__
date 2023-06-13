9612305c17528fb8e1e3427d0d9c0ddf92705481
donowhy <119389068+donowhy@users.noreply.github.com>
2023-06-13 15:52:51 +0900

    [Gold IV] Title: 불, Time: 796 ms, Memory: 221928 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,47 +1,47 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def bfs():
-    while fire:
+    while fire: 
         x, y = fire.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < h and 0 <= ny < w and building[nx][ny] != '#' and fire_spread[nx][ny] == -1:
-                fire_spread[nx][ny] = fire_spread[x][y] + 1
+            if 0 <= nx < h and 0 <= ny < w and building[nx][ny] != '#' and fire_time[nx][ny] == -1:
+                fire_time[nx][ny] = fire_time[x][y] + 1
                 fire.append((nx, ny))
-
-    while move:
-        x, y = move.popleft()
+    
+    while person: 
+        x, y = person.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= h or ny < 0 or ny >= w:
-                return move_time[x][y] + 1
-            if 0 <= nx < h and 0 <= ny < w:
-                if building[nx][ny] != '#' and move_time[nx][ny] == -1 and (fire_spread[nx][ny] == -1 or fire_spread[nx][ny] > move_time[x][y] + 1):
-                    move_time[nx][ny] = move_time[x][y] + 1
-                    move.append((nx, ny))
+            if nx < 0 or nx >= h or ny < 0 or ny >= w:  # reach the boundary
+                return person_time[x][y] + 1
+            if 0 <= nx < h and 0 <= ny < w and building[nx][ny] != '#' and person_time[nx][ny] == -1 and (fire_time[nx][ny] == -1 or fire_time[nx][ny] > person_time[x][y] + 1):
+                person_time[nx][ny] = person_time[x][y] + 1
+                person.append((nx, ny))
+    
     return "IMPOSSIBLE"
 
-T = int(input())
-for _ in range(T):
-    w, h = map(int, input().split())
-    building = [list(input().strip()) for _ in range(h)]
-    fire_spread = [[-1]*w for _ in range(h)]
-    move_time = [[-1]*w for _ in range(h)]
+t = int(sys.stdin.readline())
+for _ in range(t):
+    w, h = map(int, sys.stdin.readline().split())
+    building = [list(sys.stdin.readline().strip()) for _ in range(h)]
     fire = deque()
-    move = deque()
-
+    person = deque()
+    
+    fire_time = [[-1]*w for _ in range(h)]
+    person_time = [[-1]*w for _ in range(h)]
+    
     for i in range(h):
         for j in range(w):
             if building[i][j] == '*':
                 fire.append((i, j))
-                fire_spread[i][j] = 0
-            elif building[i][j] == '@':
-                move.append((i, j))
-                move_time[i][j] = 0
-
+                fire_time[i][j] = 0
+            if building[i][j] == '@':
+                person.append((i, j))
+                person_time[i][j] = 0
+    
     print(bfs())
