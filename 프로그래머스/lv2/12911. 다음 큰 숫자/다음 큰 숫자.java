e52fc525467294e19a80baae4dcc1920ca9c3b90
donowhy <119389068+donowhy@users.noreply.github.com>
2023-08-12 08:15:00 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,15 +1,6 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-    int countOfOnes = Integer.bitCount(n);
-    while (true) {
-        n++;
-        if (Integer.bitCount(n) == countOfOnes) {
-            return n;
-        }
+        int answer = 0;
+        return answer;
     }
-}
-
 }
