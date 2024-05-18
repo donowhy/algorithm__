5d858cdf8caeb3fb22ebed5575ee29b40b591475
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2024-05-19 04:09:47 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,34 @@
+WITH FE AS (
+    SELECT BIT_OR(CODE) AS SKILL_CODE 
+    FROM SKILLCODES 
+    GROUP BY CATEGORY 
+    HAVING CATEGORY = 'Front End'
+), DEV_GRADE AS (
+    SELECT
+        CASE
+            WHEN DEV.SKILL_CODE & FE.SKILL_CODE 
+                AND DEV.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')
+                THEN 'A'
+            WHEN DEV.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
+                THEN 'B'
+            WHEN DEV.SKILL_CODE & FE.SKILL_CODE 
+                THEN 'C'
+            ELSE NULL
+        END AS GRADE
+        , ID
+        , EMAIL
+    FROM
+        DEVELOPERS DEV
+        , FE
+)
+
+SELECT
+    GRADE
+    , ID
+    , EMAIL
+FROM
+    DEV_GRADE
+WHERE
+    GRADE IS NOT NULL
+ORDER BY
+    GRADE, ID
