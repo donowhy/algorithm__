b1deee80d391938f6643f2b079f4d3b7a43326c6
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2024-05-19 14:20:39 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 7356 ms, Memory: 290328 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,40 +1,32 @@
 import sys
 import heapq
 
-tc = int(sys.stdin.readline())
+for T in range(int(sys.stdin.readline())):
+    k = int(sys.stdin.readline())
+    visited = [False] * k
+    maxH, minH = [], []
+    for i in range(k):
+        operation, n = sys.stdin.readline().split()
+        n = int(n)
 
-for _ in range(tc):
-    visited = [False] * 1_000_001
-    min_h = []
-    max_h = []
-
-    cmd_num = int(sys.stdin.readline())
-
-    for i in range(cmd_num):
-        cmd = sys.stdin.readline().split()
-        if cmd[0] == 'I':
-            heapq.heappush(min_h, (int(cmd[1]), i))
-            heapq.heappush(max_h, (-int(cmd[1]), i))
+        if operation == 'I':
+            heapq.heappush(minH, (n, i))
+            heapq.heappush(maxH, (-n, i))
             visited[i] = True
-        elif cmd[1] == '1':
-            while max_h and not visited[max_h[0][1]]:
-                _, idx = heapq.heappop(max_h)
-            if max_h:
-                visited[max_h[0][1]] = False
-                heapq.heappop(max_h)
+        elif n == 1:
+            while maxH and not visited[maxH[0][1]]:
+                heapq.heappop(maxH)
+            if maxH:
+                visited[maxH[0][1]] = False
+                heapq.heappop(maxH)
         else:
-            while min_h and not visited[min_h[0][1]]:
-                _, idx = heapq.heappop(min_h)
-            if min_h:
-                visited[min_h[0][1]] = False
-                heapq.heappop(min_h)
-
-    while min_h and not visited[min_h[0][1]]:
-        heapq.heappop(min_h)
-    while max_h and not visited[max_h[0][1]]:
-        heapq.heappop(max_h)
-
-    if min_h and max_h:
-        print(-max_h[0][0], min_h[0][0])
-    else:
-        print("EMPTY")
+            while minH and not visited[minH[0][1]]:
+                heapq.heappop(minH)
+            if minH:
+                visited[minH[0][1]] = False
+                heapq.heappop(minH)
+    while minH and not visited[minH[0][1]]:
+        heapq.heappop(minH)
+    while maxH and not visited[maxH[0][1]]:
+        heapq.heappop(maxH)
+    print(-maxH[0][0], minH[0][0]) if maxH and minH else print("EMPTY")
