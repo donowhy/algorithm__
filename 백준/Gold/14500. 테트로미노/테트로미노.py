d14f00b0301c9ba940f3f47d7c6a7fa56897092b
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2024-06-23 22:20:10 +0900

    [Gold IV] Title: 테트로미노, Time: 872 ms, Memory: 121916 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,38 +1,64 @@
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def dfs(r, c, idx, total):
-    global ans
-    if ans >= total + max_val * (3 - idx):
-        return
-    if idx == 3:
-        ans = max(ans, total)
-        return
-    else:
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
-                if idx == 1:
-                    visit[nr][nc] = 1
-                    dfs(r, c, idx + 1, total + arr[nr][nc])
-                    visit[nr][nc] = 0
-                visit[nr][nc] = 1
-                dfs(nr, nc, idx + 1, total + arr[nr][nc])
-                visit[nr][nc] = 0
+# 상, 하, 좌, 우
+move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-
+# INPUT
 N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-visit = [([0] * M) for _ in range(N)]
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-ans = 0
-max_val = max(map(max, arr))
+board = [list(map(int,input().split())) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
 
-for r in range(N):
-    for c in range(M):
-        visit[r][c] = 1
-        dfs(r, c, 0, arr[r][c])
-        visit[r][c] = 0
+# 최대값 변수
+maxValue = 0
 
-print(ans)
+# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
+def dfs(i, j, dsum, cnt):
+    global maxValue
+    # 모양 완성되었을 때 최대값 계산
+    if cnt == 4:
+        maxValue = max(maxValue, dsum)
+        return
+
+    # 상, 하, 좌, 우로 이동
+    for n in range(4):
+        ni = i+move[n][0]
+        nj = j+move[n][1]
+        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:
+            # 방문 표시 및 제거
+            visited[ni][nj] = True
+            dfs(ni, nj, dsum + board[ni][nj], cnt+1)
+            visited[ni][nj] = False
+
+
+# ㅗ, ㅜ, ㅓ, ㅏ 모양의 최대값 계산
+def exce(i, j):
+    global maxValue
+    for n in range(4):
+        # 초기값은 시작지점의 값으로 지정
+        tmp = board[i][j]
+        for k in range(3):
+            # move 배열의 요소를 3개씩 사용할 수 있도록 인덱스 계산
+            # 012, 123, 230, 301
+            t = (n+k)%4
+            ni = i+move[t][0]
+            nj = j+move[t][1]
+
+            if not (0 <= ni < N and 0 <= nj < M):
+                tmp = 0
+                break
+            tmp += board[ni][nj]
+        # 최대값 계산
+        maxValue = max(maxValue, tmp)
+
+
+for i in range(N):
+    for j in range(M):
+        # 시작점 visited 표시
+        visited[i][j] = True
+        dfs(i, j, board[i][j], 1)
+        visited[i][j] = False
+
+        exce(i, j)
+
+print(maxValue)
