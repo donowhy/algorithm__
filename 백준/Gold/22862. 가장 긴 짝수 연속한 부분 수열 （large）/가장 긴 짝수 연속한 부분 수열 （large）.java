92989c22eeef77402f9d493cf574b0e46c90bbf0
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2024-11-18 21:19:03 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 436 ms, Memory: 88296 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -1,51 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static int n, k;
-    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
-
+        // 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]);
-        k = Integer.parseInt(s[1]);
-
-        arr = new int[n]; // 배열 초기화
-        s = br.readLine().split(" ");
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+        boolean[] arr = new boolean[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(s[i]);
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num % 2 == 0;
         }
 
-        int st = 0;
-        int en = 0;
-        int kcnt = 0; // 홀수의 개수를 추적하는 변수
-        int result = 0; // 최종 결과 값
-
-        // 투포인터 방식으로 풀기
-        while (en < n) { // en이 n보다 작을 때까지만 실행
-            
-            if (arr[en] % 2 == 0) {
-                en++;
-            } else {
-                if (kcnt < k) { // 아직 k보다 작으면 홀수 사용 가능
-                    kcnt++;
-                    en++;
-                } else { // k 이상이면 st 포인터를 움직여 홀수를 제외
-                    if (arr[st] % 2 != 0) {
-                        kcnt--;
-                    }
-                    st++;
+        // 투포인터
+        int maxLen = 0;
+        int l = 0;
+        int r = 0;
+        int ct = 0;
+        while (r < n) {
+            if (ct < k) {
+                if (!arr[r]) {
+                    ct++;
                 }
+                r++;
+                maxLen = Math.max(r - l - ct, maxLen);
+            } else if (arr[r]) {
+                r++;
+                maxLen = Math.max(r - l - ct, maxLen);
+            } else {
+                if (!arr[l]) {
+                    ct--;
+                }
+                l++;
             }
-
-            result = Math.max(result, en - st - kcnt); // 현재 길이를 비교
         }
 
-        System.out.println(result);
+        System.out.println(maxLen);
     }
 }
