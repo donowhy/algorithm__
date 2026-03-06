2624a1d25b3911cb5073944669ca06ddc9132a9e
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2026-03-06 23:54:15 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 152 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,91 +1,116 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	//  1
-	// 234
-	//  5
-	//  6
-	static int[] dice = new int[7];
-	static int n,m,x,y;
-	static int[][] map;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		y = Integer.parseInt(st.nextToken());
-		x = Integer.parseInt(st.nextToken());
-		
-		int k = Integer.parseInt(st.nextToken());
-		
-		map = new int[n][m];
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<k; i++) {
-			int d = Integer.parseInt(st.nextToken());
-			move(d);
-		}
-		
-	}
-	
-	static void move(int d) {
-		int nx = x + dx[d-1];
-		int ny = y + dy[d-1];
-		if(nx <0 || ny < 0 || nx > m-1 || ny > n-1) return;
-		roll(d, nx, ny);
-		x = nx; y = ny;
-		
-	}
+    private static int n, m, x, y, k;
+    private static int[][] map;
+    private static int[] dx = {0, 0, 0, -1, 1};
+    private static int[] dy = {0, 1, -1, 0, 0};
+    private static int[] dice = new int[7];
 
-	// 1 2 3 4 (동 서 남 북)
-	static void roll(int d, int x, int y) {
-		int tmp = dice[3];
-		switch(d) {
-		case 1:
-			dice[3] = dice[4];
-			dice[4] = dice[6];
-			dice[6] = dice[2];
-			dice[2] = tmp;
-			break;
-		case 2:
-			dice[3] = dice[2];
-			dice[2] = dice[6];
-			dice[6] = dice[4];
-			dice[4] = tmp;
-			break;
-		case 3:
-			dice[3] = dice[5];
-			dice[5] = dice[6];
-			dice[6] = dice[1];
-			dice[1] = tmp;
-			break;
-		case 4:
-			dice[3] = dice[1];
-			dice[1] = dice[6];
-			dice[6] = dice[5];
-			dice[5] = tmp;
-			break;
-		}
-		if(map[y][x] == 0) {
-			map[y][x] = dice[6];
-		} else {
-			dice[6] = map[y][x];
-			map[y][x] =0;
-		}
-		System.out.println(dice[3]);
-		
-	}
+    private static class P {
+        int x, y;
+        public P(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+
+        P p = new P(x, y);
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] orderList = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Moving moving = new Moving(); 
+
+        for(int i = 0; i < k; i++) {
+            int order = orderList[i];
+
+            int nx = p.x + dx[order];
+            int ny = p.y + dy[order];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+
+            p.x = nx;
+            p.y = ny;
+
+            switch (order) {
+                case 1: moving.east(dice); break;
+                case 2: moving.west(dice); break;
+                case 3: moving.north(dice); break;
+                case 4: moving.south(dice); break;
+            }
+
+            paste(p);
+            System.out.println(dice[1]);
+        }
+    }
+
+    public static class Moving {
+        public void east(int[] dice) {
+            int temp = dice[6];
+            dice[6] = dice[3];
+            dice[3] = dice[1];
+            dice[1] = dice[4];
+            dice[4] = temp;
+        }
+
+        public void west(int[] dice) {
+            int temp = dice[6];
+            dice[6] = dice[4];
+            dice[4] = dice[1];
+            dice[1] = dice[3];
+            dice[3] = temp;
+        }
+
+        public void north(int[] dice) {
+            int temp = dice[6];
+            dice[6] = dice[2];
+            dice[2] = dice[1];
+            dice[1] = dice[5];
+            dice[5] = temp;
+        }
+
+        public void south(int[] dice) {
+            int temp = dice[6];
+            dice[6] = dice[5];
+            dice[5] = dice[1];
+            dice[1] = dice[2];
+            dice[2] = temp;
+        }
+    }
+
+    private static void paste(P p)  {
+        if(map[p.x][p.y] == 0) {
+            map[p.x][p.y] = dice[6]; 
+        } else {
+            dice[6] = map[p.x][p.y]; 
+            map[p.x][p.y] = 0;       
+        }
+    }
 }
