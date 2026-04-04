1f9da960a414be03067425f4fa7ba25ecf6d1ea6
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2026-04-04 20:42:47 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 204 ms, Memory: 20112 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,83 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Main {
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static class Locate implements Comparable<Locate>{
-        int x;
-        int y;
-        int rupee;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while(true) {
+            cnt ++;
+            int n = Integer.parseInt(br.readLine());
 
-        public Locate (int x, int y, int rupee) {
+            if(n == 0) break;
+
+            int[][] map = new int[n][n];
+
+            for(int i=0; i<n; i++) {
+                String[] s = br.readLine().split(" ");
+                for(int j=0; j<n; j++) {
+                    map[i][j] = Integer.parseInt(s[j]);
+                }
+            }
+
+            int go = go(map);
+            System.out.printf("Problem %d: %d%n", cnt, go);
+        }
+    }
+
+    private static int go(int[][] map) {
+        int[][] dist = new int[map.length][map.length];
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map.length; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        dist[0][0] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(0, 0, map[0][0]));
+
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            int x = node.x;
+            int y = node.y;
+            int cost = node.cost;
+
+            for(int i=0; i<4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= map.length || ny < 0 || ny >= map.length) continue;
+                if(dist[nx][ny] <= cost + map[nx][ny]) continue;
+
+                dist[nx][ny] = cost + map[nx][ny];
+                pq.offer(new Node(nx, ny, dist[nx][ny]));
+            }
+        }
+
+        return dist[map.length-1][map.length-1];
+    }
+
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
+    private static class Node implements Comparable<Node> {
+        int x, y;
+        int cost;
+
+        public Node(int x, int y, int cost) {
             this.x = x;
             this.y = y;
-            this.rupee = rupee;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Locate locate) {
-            return Integer.compare(this.rupee, locate.rupee);
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = 1;
-        while (true) {
-            int N = Integer.parseInt(br.readLine());
-            if(N == 0) return;
-
-            int[][] map = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                String[] rupees = br.readLine().split(" ");
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(rupees[j]);
-                }
-            }
-
-            int lostRupee = findLowestWay(0, 0, map, N);
-            System.out.println("Problem " + testCase  + ": " + lostRupee);
-            testCase++;
-        }
-    }
-
-    private static int findLowestWay(int x, int y, int[][] map, int mapSize) {
-        PriorityQueue<Locate> locates = new PriorityQueue<>();
-        locates.add(new Locate(x, y, map[x][y]));
-        int result = Integer.MAX_VALUE;
-        boolean[][] check = new boolean[mapSize][mapSize];
-        fillFalseInCheck(check);
-        check[x][y] = true;
-
-        while(!locates.isEmpty()) {
-            Locate locate = locates.poll();
-
-            if(locate.x == mapSize - 1 && locate.y == mapSize - 1 ) {
-                result = Math.min(locate.rupee, result);
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = locate.x + dx[i];
-                int nextY = locate.y + dy[i];
-
-                if(nextX < 0 || nextX >= mapSize || nextY < 0 || nextY >= mapSize || check[nextX][nextY]) {
-                    continue;
-                }
-
-                check[nextX][nextY] = true;
-                locates.offer(new Locate(nextX, nextY, locate.rupee + map[nextX][nextY]));
-            }
-        }
-
-        return result;
-    }
-
-    private static void fillFalseInCheck(boolean[][] check) {
-        for (boolean[] fill : check) {
-            Arrays.fill(fill, false);
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
         }
     }
 }
